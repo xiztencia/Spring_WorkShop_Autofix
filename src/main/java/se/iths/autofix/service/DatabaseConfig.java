bcbad24066bfaa4e86e17d543d7658c9f135a7a0
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2020-12-07 01:45:50 +0100

    6th try to fix connect to DB on Heroku

--- src/main/java/se/iths/autofix/service/DatabaseConfig.java
+++ src/main/java/se/iths/autofix/service/DatabaseConfig.java
@@ -1,23 +1,18 @@
 package se.iths.autofix.service;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+        return new org.apache.tomcat.jdbc.pool.DataSource();
     }
 }

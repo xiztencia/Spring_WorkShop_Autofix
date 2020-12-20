4469c7169e9bb3b5e52ea30ed3a06bf3df917f4b
xiztencia <xiztencia@gmail.com>
2020-12-20 20:15:39 +0100

    test empty

--- src/main/java/se/iths/autofix/security/SecurityConfig.java
+++ src/main/java/se/iths/autofix/security/SecurityConfig.java
@@ -67,7 +67,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new SecurityEvaluationContextExtension();
     }
 
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());

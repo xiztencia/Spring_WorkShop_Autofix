66f74d07e3076c60102575bea386259afa0ec78f
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2020-12-28 21:20:58 +0100

    moved REST folder to outside src

--- rest/exceptions/EmployeeNotFoundException.java
+++ rest/exceptions/EmployeeNotFoundException.java
@@ -4,7 +4,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-public class EmployeeNotFoundException extends WebApplicationException {
+public class EmployeeNotFoundException extends Exception {
     public EmployeeNotFoundException(String message) {
         super(Response.status(Response.Status.NOT_FOUND).entity(message).type(MediaType.TEXT_PLAIN).build());
     }

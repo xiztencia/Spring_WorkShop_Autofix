9f9429fbcf3491d26fed65e59559941badd1b52b
Luis <lfgn84@gmail.com>
2021-01-03 02:40:41 -0600

    Created demo spare parts in database to expose data on Client view

---
+++ src/main/java/se/iths/autofix/view/webcontroller/ClientWebController.java
@@ -0,0 +1,20 @@
+package se.iths.autofix.view.webcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.autofix.service.SparePartService;
+
+@Controller
+public class ClientWebController {
+
+    @Autowired
+    private SparePartService sparePartService;
+
+    @GetMapping("/Client")
+    public String spareParts(Model model){
+        model.addAttribute("spareParts", sparePartService.findAllSpareParts());
+        return "Client.html";
+    }
+}
--- src/main/java/se/iths/autofix/view/webcontroller/EmployeeWebController.java
+++ src/main/java/se/iths/autofix/view/webcontroller/EmployeeWebController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.autofix.exception.ClientNotFoundException;
 import se.iths.autofix.service.ClientService;
 
 @Controller
@@ -13,7 +14,7 @@ public class EmployeeWebController {
     private ClientService clientService;
 
     @GetMapping("/admin")
-    public String clients(Model model){
+    public String clients(Model model) throws ClientNotFoundException {
         model.addAttribute("clients", clientService.findAllClients());
         return "/admin.html";
     }

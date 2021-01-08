9f784ed75f0a2fb3323d4e24de59d226f3c4bc7b
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2021-01-08 17:39:37 +0100

    modified for Heroku env

--- src/main/java/se/iths/autofix/jms/sender/Sender.java
+++ src/main/java/se/iths/autofix/jms/sender/Sender.java
@@ -1,32 +1,33 @@
 package se.iths.autofix.jms.sender;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import se.iths.autofix.jms.config.JmsConfig;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+
 
 @Component
 public class Sender {
 
     Logger logger = LoggerFactory.getLogger(Sender.class);
     RabbitTemplate rabbitTemplate;
-    private LocalDateTime localDateTime;
+
 
     public Sender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
 
-    @Scheduled(fixedRate = 2000)
-    public void sendMessage() {
-        logger.info("Message sent: Hello world");
-        rabbitTemplate.convertAndSend("TestQueue", "Det funka!!!"+ localDateTime);
+   // @Scheduled(fixedRate = 2000)
+    public void sendMessage(String message,String info) {
+        logger.info("Message sent: "+info);
+        rabbitTemplate.convertAndSend("TestQueue", message);
 
     }
 

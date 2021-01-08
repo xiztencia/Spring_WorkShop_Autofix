6958182086fc72d57408d3b1dee55bb16e38d87f
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2021-01-08 17:46:16 +0100

    modified for Heroku env

--- src/main/java/se/iths/autofix/view/webcontroller/ClientWebController.java
+++ src/main/java/se/iths/autofix/view/webcontroller/ClientWebController.java
@@ -1,9 +1,12 @@
 package se.iths.autofix.view.webcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.autofix.jms.sender.Sender;
 import se.iths.autofix.service.SparePartService;
 
 @Controller
@@ -11,9 +14,13 @@ public class ClientWebController {
 
     @Autowired
     private SparePartService sparePartService;
+    private Sender jmsSender;
 
     @GetMapping("/Client")
     public String spareParts(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        jmsSender.sendMessage(currentPrincipalName+" Logged","Loggin Event!!: ");
         model.addAttribute("spareParts", sparePartService.findAllSpareParts());
         return "Client.html";
     }

7d5a902fa070faf521407916c91d1ea46ec87450
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2021-01-08 17:30:59 +0100

    modified for Heroku env

--- src/main/java/se/iths/autofix/jms/sender/Sender.java
+++ src/main/java/se/iths/autofix/jms/sender/Sender.java
@@ -8,11 +8,15 @@ import se.iths.autofix.jms.config.JmsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Component
 public class Sender {
 
     Logger logger = LoggerFactory.getLogger(Sender.class);
     RabbitTemplate rabbitTemplate;
+    private LocalDateTime localDateTime;
 
     public Sender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
@@ -22,7 +26,7 @@ public class Sender {
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
         logger.info("Message sent: Hello world");
-        rabbitTemplate.convertAndSend("TestQueue", "Hello, world!");
+        rabbitTemplate.convertAndSend("TestQueue", "Det funka!!!"+ localDateTime);
 
     }
 
